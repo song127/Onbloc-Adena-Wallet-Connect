1bfa445f0f001711b0920b2b886ec1e4853fec0c
song127 <thdtmdwo789@naver.com>
2025-05-30 12:27:58 +0900

    refactor: remove unuse files

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,13 @@
-import React from "react";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 
 import Home from "@/pages/Home";
-import SDK from "@/pages/SDK";
 import { ToastManager } from "@/components/toast/ToastManager";
 
-const App: React.FC = () => {
+const App = () => {
   return (
     <BrowserRouter>
       <Routes>
         <Route path="/" element={<Home />} />
-        <Route path="/sdk" element={<SDK />} />
       </Routes>
       <ToastManager />
     </BrowserRouter>
